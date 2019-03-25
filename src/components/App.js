85053639b64d1e8081eeee71ab59ae8a16dffaaa
konovalov i <ivan.konovalov@noveogroup.com>
2019-03-25 15:45:23 +0700

    js-training added some base structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends Component {
+    static defaultProps = {};
+
+    static propTypes = {};
+
+    state = {};
+
+    render() {
+        return (
+            <div className="bg-warning">123123131</div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('trainingApp'));
