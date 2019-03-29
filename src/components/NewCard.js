92b725959021b36126171899ab13229d4565dd65
konovalov i <ivan.konovalov@noveogroup.com>
2019-03-29 10:23:10 +0700

    react-training added new card component

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,42 +1,39 @@
-import React, {useCallback} from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
-import {Redirect} from "react-router-dom"
+import { Redirect } from 'react-router-dom'
+import {MAX_TITLE_LENGTH, MAX_TEXT_LENGTH, validateTitle, validateText} from "../constants/validation"
 import useCard from "../hooks/useCard"
 import useValidation from "../hooks/useValidation"
+import Validator from "../validators/Validator"
 import Title from "./Title"
 import {SimpleInput, SimpleTextarea} from "./forms/inputs"
-import {MAX_TEXT_LENGTH, MAX_TITLE_LENGTH, validateText, validateTitle} from "../constants/validation"
-import Validator from "../validators/Validator"
 
 /**
- * Card component. Allows to edit card information and delete card. Takes change and remove callbacks
+ * New Card component. Allows to edit card information and save card. Takes save callback
  * Uses card hook and validation hook. Receives as a prop validators object with specific validator for each field
  * when complete redirects to main page
  */
-const Card = React.memo(({ card: {id, ...card}, change, remove, fieldsValidators }) => {
+const NewCard = React.memo(({ save, fieldsValidators }) => {
 
-    const { title, text, complete, setComplete, changeTextHandler, changeTitleHandler } = useCard(card)
+    const { title, text, complete, setComplete, changeTextHandler, changeTitleHandler } = useCard()
     const { invalidFields, validate } = useValidation(fieldsValidators)
 
     const isValid = useCallback(field => !invalidFields.includes(field), [invalidFields])
-    const removeHandler = useCallback(() => {
-        setComplete(true)
-        remove(id)
-    }, [])
     const saveHandler = useCallback(() => {
-        if (validate({ title, text })) {
-            change(id, { title, text })
-            setComplete(true)
-        }
+       if (validate({ title, text })) {
+           save({ title, text })
+           setComplete(true)
+       }
     },[title, text])
 
     return complete
         ? <Redirect to='/' />
         : <div>
-            <Title>Edit Card</Title>
+            <Title>New Card</Title>
             <div className='card newCard shadow'>
                 <div className="card-header">
                     <SimpleInput value={title}
+                        placeholder='Enter title'
                         maxLength={MAX_TITLE_LENGTH}
                         onChange={changeTitleHandler}
                         className={isValid('title') ? '' : 'is-invalid'}
@@ -44,39 +41,29 @@ const Card = React.memo(({ card: {id, ...card}, change, remove, fieldsValidators
                 </div>
                 <div className="card-body">
                     <SimpleTextarea value={text}
+                        placeholder='Enter text'
                         maxLength={MAX_TEXT_LENGTH}
                         onChange={changeTextHandler}
                         className={isValid('text') ? '' : 'is-invalid'}
                     />
-                    <div className="btn-group btn-group-lg btn-block">
-                        <button type="button" className="btn btn-secondary mt-3" onClick={saveHandler}>
-                            Save
-                        </button>
-                        <button type="button" className="btn btn-danger mt-3" onClick={removeHandler}>
-                            Delete
-                        </button>
-                    </div>
+                    <button type="button" className="btn btn-lg btn-secondary mt-3 btn-block" onClick={saveHandler}>
+                        Save
+                    </button>
                 </div>
             </div>
         </div>
 })
 
-Card.defaultProps = {
+NewCard.defaultProps = {
     fieldsValidators: {
         'title': new Validator(validateTitle),
         'text': new Validator(validateText)
     }
 }
 
-Card.propTypes = {
-    card: PropTypes.shape({
-       id: PropTypes.number,
-       title: PropTypes.string,
-       text: PropTypes.string
-    }).isRequired,
-    remove: PropTypes.func.isRequired,
-    change: PropTypes.func.isRequired,
+NewCard.propTypes = {
+    save: PropTypes.func.isRequired,
     fieldsValidators: PropTypes.objectOf(PropTypes.instanceOf(Validator))
 }
 
-export default Card
+export default NewCard
